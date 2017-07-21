c6d423f1149ae343ede9da956adc42c9610c50cd
Ashok Dey <ashokdey100@gmail.com>
2017-07-22 05:08:11 +0530

    Added react-mdl  components and  relatedfiles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {Layout, Header, Content, Navigation, Drawer} from 'react-mdl';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Layout fixedHeader>
+          <Header title="ProductHuntPWA">
+            <Navigation className="hide-on-sm"></Navigation>
+          </Header>
+          <Drawer title="ProductHubt">
+            <Navigation></Navigation>
+          </Drawer>          
+          <Content>
+            Dynamic  content here 
+          </Content>
+        </Layout>
       </div>
     );
   }
