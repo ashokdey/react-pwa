502f317363f4ccaeedee73145836c8bf78f9fe1c
Ashok Dey <ashokdey100@gmail.com>
2017-07-22 06:43:55 +0530

    caching layout files

---
+++ public/sw.js
@@ -0,0 +1,9 @@
+const cacheName = 'product-hunt';
+
+self.addEventListener('install',  event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache  => cache.addAll[
+      '/static/js/bundle.js','/index.html', '/'
+    ])
+  );
+});
