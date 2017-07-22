419d7b2b9000211f940d8785f594f49044a588f8
Ashok Dey <ashokdey100@gmail.com>
2017-07-22 07:52:44 +0530

    Added routing using react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import {Layout, Header, Content, Navigation, Drawer} from 'react-mdl';
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Layout fixedHeader>
-          <Header title="ProductHuntPWA">
-            <Navigation className="hide-on-sm"></Navigation>
-          </Header>
-          <Drawer title="ProductHunt">
-            <Navigation></Navigation>
-          </Drawer>          
-          <Content>
-            Dynamic  content here 
-          </Content>
-        </Layout>
+        <Router>
+            <Layout fixedHeader>
+              <Header title="ProductHuntPWA">
+                <Navigation className="hide-on-sm">
+                  <Link to="tech">Tech products</Link>
+                  <Link to="games">Game products</Link>
+                </Navigation>
+              </Header>
+              <Drawer title="ProductHunt">
+                <Navigation></Navigation>
+              </Drawer>          
+              <Content>
+                Dynamic  content here 
+              </Content>
+            </Layout>
+        </Router>
       </div>
     );
   }
