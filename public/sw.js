ba5033888add502eef351df41b0ab90bfb8f6f86
Ashok Dey <ashokdey100@gmail.com>
2017-07-22 06:55:55 +0530

    fix missing braces. Caching  working fine now

--- public/sw.js
+++ public/sw.js
@@ -2,8 +2,12 @@ const cacheName = 'product-hunt';
 
 self.addEventListener('install',  event => {
   event.waitUntil(
-    caches.open(cacheName).then(cache  => cache.addAll[
-      '/static/js/bundle.js','/index.html', '/'
-    ])
+    caches.open(cacheName).then(cache  => cache.addAll(
+      [
+        '/static/js/bundle.js',
+        '/index.html', 
+        '/'
+      ]
+    ))
   );
 });
