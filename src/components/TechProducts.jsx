cc9fd84ff7e863f825b8154a4aa0f81fddafbd51
Ashok Dey <ashokdey100@gmail.com>
2017-07-22 17:00:56 +0530

    fetching data  from api

--- src/components/TechProducts.jsx
+++ src/components/TechProducts.jsx
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import 'whatwg-fetch';
 
+const KEY = 'Bearer 4bf782db360dd58f4411996195a4c3173e058cd5f14e169dfb31387c4701fe48';
+const URL = 'https://api.producthunt.com/v1/categories/tech/posts';
 
 export default class TechProduct extends Component {
+  componentDidMount() {
+    fetch(URL, {
+      method: 'GET',
+      headers: {
+        'Authorization': KEY
+      }
+    }).then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  }
   render() {
     return (
       <div>
